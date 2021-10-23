f1fe27edcfa2cf1309caf5311e9f4ba32f0488df
mounntainn <TakumaYamashita_FSE@outlook.jp>
2021-10-24 00:44:30 +0900

    API修正

--- src/index.ts
+++ src/index.ts
@@ -11,29 +11,66 @@ if (process.env.NODE_ENV !== 'production') {
     require('dotenv').config();
 }
 
-// space対応の為postに変更 
-app.get('/', async (req: any, res: any) => {
-    var params = {
-        q: req.query.words,
-        lang: 'en',
-        result_type: 'popular',
-        count: 10,
-        tweet_mode: 'extended',
-        include_entities: false
-    };
-    
-    const results = await getTweets(req, res, params);
-    // console.log(results);
-    for (let result of results) {
-        // console.log(result);
-        result['jaText'] = await translate(result.text, 'en', 'ja');
-    }
-    res.send(results);
+
+
+
+
+
+
+
+
+
+
+let twitterResult: any = [];
+
+
+
+
+
+
+
+
+
+
+
+
+// todo: space対応の為postに変更 
+app.get('/', (req: any, res: any, next: any) => {
+    (async () => {
+        console.log('start api.');
+        
+        if (!req.query.words) {
+            console.log('words are required.');
+            res.send('words are required.');
+            return;
+        }
+
+        var params = {
+            q: req.query.words,
+            lang: 'en',
+            result_type: 'popular',
+            count: 10,
+            tweet_mode: 'extended',
+            include_entities: false
+        };
+        
+        // const results = await getTweets(req, res, params);
+        await getTweets(req, res, params);
+        console.log(twitterResult);
+        console.log('twitterResult');
+        for (let result of twitterResult) {
+            // console.log(result);
+            result['jaText'] = await translate(result.text, 'en', 'ja');
+        }
+        res.send(twitterResult);
+
+        console.log('end api.');
+    })().catch(next);
 });
 
-app.listen(port, () => console.log(`Successfully served app listening http://${host}:${port}`));
+app.listen(port, () => console.log(`Successfully served http://${host}:${port}`));
 
-let twitterResult: any = [];    
+
 async function getTweets(req: any, res: any, params: any): Promise<any> {
     const twitterClient = new Twitter({
       consumer_key: process.env.TWITTER_CONSUMER_KEY,
@@ -43,39 +80,35 @@ async function getTweets(req: any, res: any, params: any): Promise<any> {
     });
 
     console.log("requested words:", req.query.words);
-    await twitterClient.get('/search/tweets.json', params, (error: any, tweets: any, response: any) => {
-        if (error) {
-            console.log(error);
-            return;
-        }
-
-        if (response.statusCode !== 200) {
+    twitterClient.get('/search/tweets.json', params)
+        .then((tweets: any, response: any) => {
+            // if (error) {
+            console.log(tweets);
             console.log(response);
-            console.log('response error');
-        }
+            //     return;
+            // }
 
-        console.log("twitter api query:", params.q);
-        
-        const datas = tweets.statuses;
-        const results = datas
-            // .filter((x: any) => x.text.match(`/${query}/`))
-            .map((x: any) => {
-                // console.log(x);
-                return {
-                    name: x.user.name,
-                    text: x.full_text
-                };
-            });
-        twitterResult = results;
-        // console.log(twitterResult);
-        // console.log(results);
-    });
+            // if (response.statusCode !== 200) {
+            //     console.log(response);
+            //     console.log('response error');
+            // }
 
-    // console.log(twitterResult);
-    return twitterResult;
+            console.log("twitter api query:", params.q);
+            
+            const datas = tweets.statuses;
+            for (let x of datas) {
+                twitterResult.push(
+                    {
+                        name: x.user.name,
+                        text: x.full_text
+                    }
+                );
+            }
+        });
 }
 
 async function translate(text: any, sourceLang: any, targetLang: any) {
+    // console.log('tran');
     const apiKey = process.env.GCP_TRANSLATE_API_KEY;
     const projectId = process.env.GCP_PROJECT_ID;
     const location = process.env.GCP_LOCATION;
@@ -88,10 +121,13 @@ async function translate(text: any, sourceLang: any, targetLang: any) {
         sourceLanguageCode: sourceLang,
         targetLanguageCode: targetLang,
     };
-    const res = await translationClient.translateText(req);
-    for (const elem of res) {
-        if (elem == null) // なぜかnullがレスポンスに含まれる
-            continue;
-        return elem["translations"][0]["translatedText"];
+    const reses = await translationClient.translateText(req);
+    
+    for (const res of reses) {
+        if (res == null) {
+            continue; // なぜかnullがレスポンスに含まれる
+        }
+        console.log(res);
+        return res["translations"][0]["translatedText"];
     }
 }
